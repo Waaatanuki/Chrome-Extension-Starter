6d218b7501acb950a5063b8837d18bf939a09b84
Waaatanuki <watanukipublic@gmail.com>
2023-08-28 14:58:32 +0800

    feat: 增加打包脚本

---
+++ scripts/zip.ts
@@ -0,0 +1,24 @@
+import fs from 'node:fs'
+import archiver from 'archiver'
+import manifest from '../manifest.json'
+
+function zipDirectory(dirpath: string, destpath: string, outPath: string) {
+  const archive = archiver('zip', { zlib: { level: 9 } })
+  const stream = fs.createWriteStream(outPath)
+
+  return new Promise<void>((resolve, reject) => {
+    archive
+      .directory(dirpath, destpath)
+      .on('error', err => reject(err))
+      .pipe(stream)
+
+    stream.on('close', () => resolve())
+    archive.finalize()
+  })
+}
+
+const dirpath = 'dist'
+const destpath = manifest.name
+const zipFilePath = `${destpath} v${manifest.version}.zip`
+
+zipDirectory(dirpath, destpath, zipFilePath)
