12382f8c0c4b0cc161abed622dd896fc5eff92ee
Waaatanuki <watanukipublic@gmail.com>
2024-03-20 09:07:16 +0800

    chore: 更新依赖

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,3 @@
 import antfu from '@antfu/eslint-config'
-import unocss from '@unocss/eslint-plugin'
 
-export default antfu(
-  {},
-  unocss.configs.flat,
-)
+export default antfu({ unocss: true })
