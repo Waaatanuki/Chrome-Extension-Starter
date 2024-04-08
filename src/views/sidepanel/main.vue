bb2839a659f4bb41364d6a96cc34ee2a23c911dd
Waaatanuki <watanukipublic@gmail.com>
2024-04-08 16:26:18 +0800

    feat: 增加侧边栏配置

---
+++ src/views/sidepanel/main.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { storageDemo } from '~/logic/storage'
+
+function hi() {
+  ElMessage.success(storageDemo.value)
+}
+</script>
+
+<template>
+  <div mx-auto fc flex-col gap-5 p-10>
+    <div w-full flex items-center justify-end>
+      <div class="icon-btn" i-carbon-sun dark:i-carbon-moon @click="toggleDark()" />
+    </div>
+    <h1>Hello {{ storageDemo }}!</h1>
+    <el-input v-model="storageDemo" />
+    <div btn type="primary" @click="hi">
+      👋
+    </div>
+  </div>
+</template>
